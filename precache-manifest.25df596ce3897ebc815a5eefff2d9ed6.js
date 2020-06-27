66d3395447367fb893d97f5051636505060beda8
palak-porwal <palakporwal97@gmail.com>
2020-06-27 10:08:40 -0700

    Updates

---
+++ precache-manifest.25df596ce3897ebc815a5eefff2d9ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17c3fc33f4edda050a72a43ef7cb50b",
+    "url": "/github.io/monster/index.html"
+  },
+  {
+    "revision": "48ce14a13c6ac16df683",
+    "url": "/github.io/monster/static/css/main.757ceadf.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/github.io/monster/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monster/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ce14a13c6ac16df683",
+    "url": "/github.io/monster/static/js/main.d26cf4db.chunk.js"
+  },
+  {
+    "revision": "03cf8e8aa74032e8c532",
+    "url": "/github.io/monster/static/js/runtime-main.3c0c2bba.js"
+  }
+]);
